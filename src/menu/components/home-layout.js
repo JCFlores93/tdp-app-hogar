381b82caf198defcd0a2a28f214d3a49e87af864
jeanCarlo <jeancarlo.flores@vasslatam.com>
2018-04-19 17:07:11 -0500

    modifying style

--- src/menu/components/home-layout.js
+++ src/menu/components/home-layout.js
@@ -1,9 +1,80 @@
 import React from 'react'
 
-function HomeLayout(props) {
+selector = props => (
+    (
+        <div>
+            <select
+                name="country"
+                multiple={false}
+                value={this.props.list}
+                onChange={this.props.onChange}
+            >
+                {
+                    this.state.listCode.map((e, key) => {
+                        return <option
+                            key={key}
+                            value={e.plan}
+                        >
+                            {e.code}
+                        </option>
+                    })
+                }
+            </select>
+        </div>
+    )
+)
+
+tabsCollection = props => (
+    (
+        <div>
+            <div className="tab">
+                <button className="tablinks" ref={button => this.button = button} onClick={event => this.openCity(event, 'London')} id="defaultOpen">London</button>
+                <button className="tablinks" onClick={event => this.openCity(event, 'Paris')}>Paris</button>
+                <button className="tablinks" onClick={event => this.openCity(event, 'Tokyo')}>Tokyo</button>
+            </div>
+            <div id="London" class="tabcontent">
+                <h3>London</h3>
+                <p>London is the capital city of England.</p>
+            </div>
+
+            <div id="Paris" class="tabcontent">
+                <h3>Paris</h3>
+                <p>Paris is the capital of France.</p>
+            </div>
+
+            <div id="Tokyo" class="tabcontent">
+                <h3>Tokyo</h3>
+                <p>Tokyo is the capital of Japan.</p>
+            </div>
+        </div>
+    )
+)
+
+
+
+planView = props => (
+    (
+        <div>
+            {/*<p>Tu Plan: {props.product.name}</p>
+            <p>Producto1: {props.product.product1}</p>
+            <p>Producto2: {props.product.product2}</p>
+    <p>Producto3: {props.product.product3}</p>*/}
+            {/*<button onClick={props.showAlert}>Press me!</button>*/}
+        </div>
+    )
+)
+
+
+
+
+
+/*function HomeLayout(props) {
     return (
         <div></div>
     )
 }
 
-export default HomeLayout
+Selector() = props => {
+
+}*/
+export { selector, tabsCollection, planView }
--- src/menu/containers/home.js
+++ src/menu/containers/home.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import HomeLayout from '../components/home-layout'
+//import HomeLayout from '../components/home-layout'
 import DebtLayout from '../components/debt-layout'
 import CurrentPlanLayout from '../components/current-plan-layout'
+import '../components/home-layout.css'
+import {
+    selector as SelectorLayout,
+    tabsCollection as Tab,
+    planView as MainPlan
+} from '../components/home-layout'
 
 class Home extends Component {
+
+    componentDidMount() {
+
+    }
     state = {
 
         listCode: [
@@ -19,7 +29,7 @@ class Home extends Component {
             date: "27 de Febrero"
         },
         product: {
-            name:"trío",
+            name: "trío",
             product1: "Internet 6 mbps",
             product2: "Internet 6 mbps",
             product3: "Internet 6 mbps"
@@ -33,36 +43,73 @@ class Home extends Component {
     showAlert = () => {
         console.log("It's working")
     }
+
+    componentDidMount() {
+        document.getElementById("defaultOpen").click()
+        /*let defaultTab = this.refs.defaultOpen
+        defaultTab.click()*/
+        //this.button.click()
+        //console.log("1")
+    }
+
+    openCity = (event, cityName) => {
+        console.log("event ", event)
+        console.log("cityName  ", cityName)
+        // Get the element with id="defaultOpen" and click on it
+        let tabcontent = document.getElementsByClassName("tabcontent")
+        for (let i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none"
+        }
+        let tablinks = document.getElementsByClassName("tablinks")
+        for (let i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "")
+        }
+        document.getElementById(cityName).style.display = "block"
+        event.currentTarget.className += " active"
+    }
+
     render() {
+
         return (
-
             <div>
-                
-                <h1>Mi Movistar</h1>
-                <select
-                    name="country"
-                    multiple={false}
-                    value={this.state.listCode}
-                    onChange={event => (this.changeList(event))}
+                <Tab
+                    onClick={this.openCity()}
                 >
-                    {
-                        this.state.listCode.map((e, key) => {
-                            return <option
-                                key={key}
-                                value={e.plan}
-                            >
-                                {e.code}
-                            </option>
-                        })
-                    }
+                    <SelectorLayout
+                        list={this.state.listCode}
+                        onChange={event => (this.changeList(event))}
+                    />
+                    <MainPlan />
 
-                </select>
-                    {/*TODO:
+                </Tab>
+                {/*
+                <div className="tab">
+                    <button className="tablinks" ref={button => this.button = button} onClick={event => this.openCity(event, 'London')} id="defaultOpen">London</button>
+                    <button className="tablinks" onClick={event => this.openCity(event, 'Paris')}>Paris</button>
+                    <button className="tablinks" onClick={event => this.openCity(event, 'Tokyo')}>Tokyo</button>
+                </div>
+                <div id="London" class="tabcontent">
+                    <h3>London</h3>
+                    <p>London is the capital city of England.</p>
+                </div>
+
+                <div id="Paris" class="tabcontent">
+                    <h3>Paris</h3>
+                    <p>Paris is the capital of France.</p>
+                </div>
+
+                <div id="Tokyo" class="tabcontent">
+                    <h3>Tokyo</h3>
+                    <p>Tokyo is the capital of Japan.</p>
+                </div>
+
+                <h1>Mi Movistar</h1>
+                */}
+
+                {/*TODO:
                     *CREATE TABS AND SLIDER AND PASS ALL THERE.
                 
                 */}
-
-
                 <HomeLayout />
                 <DebtLayout
                     debts={this.state.debts}
